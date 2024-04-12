3872572191e3c155f9786f4621af388ece678dc9
FlorianNaef <florian.naef@protonmail.com>
2024-04-12 22:58:54 +0200

    add quote layout

--- startpage/assets/js/quotes.js
+++ startpage/assets/js/quotes.js
@@ -1,12 +1,16 @@
-const containerElement = document.querySelector('.quotesBlock');
-
-displayQuote();
+function getQuote() {
+  const url =
+    'https://api.allorigins.win/raw?url=https://zenquotes.io/api/random';
+  return fetch(url).then((r) => r.json());
+}
 
 function displayQuote() {
-  const url = 'https://api.allorigins.win/raw?url=https://zenquotes.io/api/random';
-  fetch(url)
-    .then((r) => r.json())
-    .then((r) => {
-      containerElement.innerHTML = '<p>' + r[0].q + ' - ' + r[0].a + '</p>';
-    });
+  const spanText = document.querySelector('.quoteText');
+  const spanAutor = document.querySelector('.quoteAutor');
+  getQuote().then((r) => {
+    spanText.innerText = r[0].q;
+    spanAutor.innerText = r[0].a;
+  });
 }
+
+displayQuote();
